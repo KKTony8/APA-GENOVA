81ab7f676f80647337b43189c864c83c161bf1b1
OliverW <a163235304@163.com>
2025-05-26 11:34:01 +0800

    Update Old_APA.R

--- Loop/APA/Old_APA.R
+++ Loop/APA/Old_APA.R
@@ -6,6 +6,11 @@ shared_loop_bedpe <- "/data5/Wuky/CHIFQ/software/YOResult/itx/loops_bedpe/old.lo
 young_mcool_path  <- "/data5/Wuky/CHIFQ/software/YOResult/hic60/mcool/yHSC-4.mcool"
 old_mcool_path    <- "/data5/Wuky/CHIFQ/software/YOResult/hic60/mcool/oHSC-78.mcool"
 
+### 读取mcool文件之前需要计算每个分辨率的权重，正常！！！
+##conda activate cooler_env
+#hic2cool convert yHSC5.hic yHSC5.mcool -r 0
+#cooler balance /data5/Wuky/CHIFQ/software/YOResult/hic60/mcool/oHSC-7.mcool::/resolutions/10000
+
 # 读取 mcool 文件
 yHSC_4_10kb <- load_contacts(signal_path = young_mcool_path,
                              sample_name = "yHSC_4",
