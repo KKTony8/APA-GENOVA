12d368f6de2c08d92e9df19f2e62751f5c853aaa
OliverW <a163235304@163.com>
2025-06-09 15:25:39 +0800

    Update 1stat_TAD.sh

--- TAD/1stat_TAD.sh
+++ TAD/1stat_TAD.sh
@@ -1,6 +1,6 @@
 ####22:55 2025/6/8 wky
 
-###第一步转换hic文件：对hic2cool转换.hic文件为.mcool文件
+###第一步： 转换hic文件：对hic2cool转换.hic文件为.mcool文件
 ###第二步：查找TAD，运用hicexplorer软件中hicFindTADs
 
 #!/bin/bash
@@ -24,7 +24,7 @@ hicFindTADs \
   --minDepth 300000 \
   --maxDepth 600000
 
-###第三步绘制TAD长度与数量的关系图
+###第三步： 绘制TAD长度与数量的关系图
 
 library(tidyverse)
 
@@ -59,3 +59,31 @@ tads_summary <- all_tads %>%
 
 # 输出结果
 print(tads_summary)
+
+####第四步： 绘制TAD长度分布密度图
+#密度图
+library(tidyverse)
+
+# 转换长度为 Mb
+all_tads <- all_tads %>%
+  mutate(length_Mb = length / 1e6)
+
+# 密度线图（去掉填充）
+ggplot(all_tads, aes(x = length_Mb, color = sample)) +
+  geom_density(size = 1.2) +
+  scale_x_continuous(limits = c(0, 5)) +
+  labs(title = "TAD Length Distribution",
+       x = "TAD Length (Mb)",
+       y = "Density",
+       color = "Sample") +
+  theme_minimal()
+
+###*直方图
+ggplot(all_tads, aes(x = length / 1e6, fill = sample)) +
+  geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.2) +
+  scale_x_continuous(limits = c(0, 5)) +
+  labs(title = "TAD Length Histogram",
+       x = "TAD Length (Mb)",
+       y = "Count",
+       fill = "Sample") +
+  theme_minimal()
